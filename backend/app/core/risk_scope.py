ef83174a9e741d185b81b0f4b7e4e728afca4235
Krishna Katiyar <katiyaarkrishna@gmail.com>
2026-09-05 01:07:32 +0530

    adding a second deterministic feature

---
+++ backend/app/core/risk_scope.py
@@ -0,0 +1,29 @@
+CUSTOMER_DATA_KEYWORDS = [
+    "customer data",
+    "customer records",
+    "customer information",
+]
+
+FINANCIAL_DATA_KEYWORDS = [
+    "financial data",
+    "bank account",
+    "credit card",
+    "payment data",
+]
+
+EMPLOYEE_DATA_KEYWORDS = [
+    "employee data",
+    "employee records",
+    "employee information",
+]
+
+DATABASE_KEYWORDS = [
+    "database",
+    "db",
+]
+
+TEMPORARY_FILE_KEYWORDS = [
+    "temporary files",
+    "temp files",
+    "temporary data",
+]
