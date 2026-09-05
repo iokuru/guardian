c6af7321af497113ee6258eaee44817d22c6f30e
Krishna Katiyar <katiyaarkrishna@gmail.com>
2026-09-05 17:17:08 +0530

    adding db module

---
+++ backend/app/models/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+DATABASE_URL = "postgresql://guardian:guardian@localhost:5432/guardian"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+)
+
+Base = declarative_base()
